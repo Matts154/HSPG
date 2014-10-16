59101f735f03dc7d49daf84403039faa1e3dc7cd
Matt <bored154@gmail.com>
2014-10-16 00:43:57 -0700

    Updated comments

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ using namespace std;
 int main(){
     unsigned int input;
     string keyboard[2] = {"123456!@#$qwertasdfgzxcvb",	// Left half of the keyboard
-                          "7890yuiophjklnm[]"};			// Right half
+                          "7890yuiophjklnm[]"};		// Right half
 
     srand(time(NULL));
 
     do {
-		// Prompt the user for a password length
+	// Prompt the user for a password length
         cout << "\nNum: ";
         cin >> input;
         cin.ignore();
 
         for(unsigned int i = 0; i < input; i++)
-            cout << keyboard[i%2][rand() % keyboard[i%2].length()]; // First array index changes sides of the keyboard.
-																	// Second array index chooses a random character.
+            cout << keyboard[i%2][rand() % keyboard[i%2].length()]	// First array index changes sides of the keyboard.
+									// Second array index chooses a random character.
         cout << "\n\nAgain? [y/n]: ";
     } while(getchar() == 'y');
 
